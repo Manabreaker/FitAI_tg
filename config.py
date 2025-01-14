1e54331d18a6735b394edb822808b1c9694255b3
Manabreak <49814438+Manabreaker@users.noreply.github.com>
2025-01-14 10:34:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 TestMode = True
-TOKEN = "7389014384:AAELuvU5Rel2JUaO13v4SWI-lqkl94yQ_6k"
-g4f_model_name = 'gpt-4'
+TOKEN = "YOUR_BOT_TOCKEN"
+g4f_model_name = 'gpt-4' # allowed models: gpt4, gpt-4o, gpt-4o-mini, gpt3.5
+user_data_path = 'user_data'
