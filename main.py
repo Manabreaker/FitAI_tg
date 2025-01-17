8cb7675acf5606838c8a874b6e16e865436825e4
yzmooz <159586099+yzmooz@users.noreply.github.com>
2025-01-17 22:16:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from db import init_db
 from init_bot import bot, dp
 from handlers.registration import registration_router
 from handlers.menu import menu_router
+from fit_ai import schedule_existing_notifications
 
 async def main():
     # Инициализация БД
     init_db()
-
+    schedule_existing_notifications()
     # Подключаем роутеры
     dp.include_router(registration_router)
     dp.include_router(menu_router)
